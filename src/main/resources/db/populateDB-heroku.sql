6408e4d74bf4b90a82e9b559bb4473b74697c121
f9208 <63202861+f9208@users.noreply.github.com>
2022-01-12 00:26:23 +0300

    Update subscribe restaurants and menu, add label-images

--- src/main/resources/db/populateDB-heroku.sql
+++
@@ -1,7 +0,0 @@
-ALTER SEQUENCE USER_SEQ RESTART WITH 10000;
-
-INSERT INTO users (name, email, password)
-VALUES ('Admin', 'admin@gmail.com', '{noop}12345'),
-       ('Jonny', 'jonny@gmail.com', '{noop}passwordJonny'),
-       ('Ket', 'kety@gmail.com', '{noop}passwordKety'),
-       ('leo', 'leonard@gmail.com', '{noop}12345');
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -11,29 +11,62 @@ VALUES ('Admin', 'admin@gmail.com', '{noop}12345'),
        ('leo', 'leonard@gmail.com', '{noop}12345');
 
 INSERT INTO image_labels(name, link_reduced)
-VALUES ('Cone wood', '/resources/pic/cone.JPG'),
-       ('Blueberry swamp', '/resources/pic/blueberry_on_table.JPG'),
-       ('Vegetarian heaven', '/resources/pic/grass.JPG'),
-       ('Village bar', '/resources/pic/onion_in_box.JPG'),
-       ('Cafe-bar', '/resources/pic/strawberry.JPG');
+VALUES ('KFC', '/resources/pic/KFC.JPG'),
+       ('Блинная Mr. Pancake', '/resources/pic/Pancake.jpg'),
+       ('Бургерная The Woods', '/resources/pic/Burger.jpg'),
+       ('Хинкальная', '/resources/pic/khinkali.jpg'),
+       ('кафе-бар "Виктория"', '/resources/pic/strawberry.JPG');
 
 INSERT INTO restaurant(name, image_id, description, enabled)
-values ('Cone wood', 1, 'Маленький ресторанчик в лесу', true),
-       ('Blueberry swamp', 2, 'маленький ресторанчик на болоте', true),
-       ('Vegetarian heaven', 3, 'рай для вегетарианца', false),
-       ('Village bar', 4, 'деревенский кабак в центральном урюпинске', true),
-       ('Cafe-bar', 5, 'коктейль-бар', false);
+values ('KFC', 1,
+        ' американская сеть ресторанов фастфуда общественного питания, специализирующихся на блюдах из курятины. Была основана в 1952 году Харландом Сандерсом под вывеской Kentucky Fried Chicken',
+        true),
+       ('Блинная Mr. Pancake', 2,
+        'Маленькое семейное кафе расположенное в самом центре нашего города! Здесь всегда свежие, горячие и вкусные блины с различной начинкой! Также в меню есть горячие напитки!',
+        true),
+       ('Бургерная The Woods', 3,
+        'The Woods Craft Bar –это истинная атмосфера мужественности. Терпкая смола, пряная хвоя, хмель и солод - вот, что отличает нас',
+        false),
+       ('Хинкальная', 4,
+        'Где можно отведать блюда настоящей грузинской кухни? Где можно хорошо отдохнуть, получить новые впечатления, положительные эмоции? Конечно, в кафе «Хинкальная»!',
+        true),
+       ('кафе-бар "Виктория"', 5,
+        'Бар - наливайка 24 часа! Поздно возвращаешься с работы и магазины уже закрыты? Заходи!', false);
 
 INSERT INTO dishes (name, price, restaurant_id)
-VALUES ('borscht', 40, 10000),
-       ('cutlet', 120, 10000),
-       ('compote', 20, 10000),
-       ('pasta', 100, 10000),
-       ('pancake', 30, 10000),
-       ('soup', 120, 10001),
-       ('fish', 160, 10001),
-       ('potato', 100, 10001),
-       ('muffin', 55, 10001);
+VALUES ('Cheef burger', 140, 10000),
+       ('basket free', 120, 10000),
+       ('Chicken nuggets', 120, 10000),
+       ('Wings', 100, 10000),
+       ('Coca-Cola', 50, 10000),
+
+       ('Pancake', 80, 10001),
+       ('jam', 20, 10001),
+       ('green tea', 100, 10001),
+       ('black tea', 55, 10001),
+       ('green tea', 65, 10001),
+       ('cappuccino', 120, 10001),
+       ('sour cream', 50, 10001),
+       ('ice cream', 150, 10001),
+
+       ('meat burger', 500, 10002),
+       ('chicken burger', 300, 10002),
+       ('vegetarian burger', 400, 10002),
+       ('lager', 500, 10002),
+       ('black coffee', 200, 10002),
+       ('light beer', 500, 10002),
+
+       ('khinkali', 700, 10003),
+       ('dumplings', 700, 10003),
+       ('sour cream', 150, 10003),
+       ('vodka "Absolut"', 250, 10003),
+       ('sour cream', 150, 10003),
+       ('roast beef', 850, 10003),
+
+       ('beer', 150, 10004),
+       ('lays', 100, 10004),
+       ('pringles', 300, 10004),
+       ('dumplings', 500, 10004);
 
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 10000),
